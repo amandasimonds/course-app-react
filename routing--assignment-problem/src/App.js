27c51b2258dfd5badad7b8edd909fac251418612
Amanda Tree <amandasimonds9@gmail.com>
2020-06-07 15:26:33 -0600

    routed pages

--- routing--assignment-problem/src/App.js
+++ routing--assignment-problem/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import {BrowserRouter, Route, NavLink} from "react-router-dom"
+
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
 
 class App extends Component {
   render () {
     return (
+      <BrowserRouter>
       <div className="App">
         <ol style={{textAlign: 'left'}}>
           <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
@@ -17,7 +20,24 @@ class App extends Component {
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
         </ol>
+        <ul>
+          <nav>
+            <li>
+            <NavLink to="/users">
+              Users
+            </NavLink>
+            </li>
+            <li>
+            <NavLink to="/courses">
+              Courses
+            </NavLink>
+            </li>
+          </nav>
+        </ul>
+        <Route path ="/users" component={Users}/>
+        <Route path="/courses" component={Courses}/>
       </div>
+      </BrowserRouter>
     );
   }
 }
